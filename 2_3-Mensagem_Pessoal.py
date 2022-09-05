d526e99195c544295639dca042d5593f0787f9a4
FRANCISCO EVALDO <evaldocomp@gmail.com>
2022-09-05 16:14:14 -0300

    teste

---
+++ 2_3-Mensagem_Pessoal.py
@@ -0,0 +1,5 @@
+'''Armazene o nome de uma pessoa em uma variável e apresente uma mensagem a essa pessoa. Sua mensagem deve
+ser simples, como “Alô Eric, você gostaria de aprender um pouco de Python hoje?”'''
+
+nome = input("Digite seu nome\t")
+print("Alô ",nome," você gostaria de aprender um pouco de Python hoje?")
